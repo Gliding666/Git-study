32be8656371451f20c9c228bb319426de7a54685
Gliding666 <2225819610@qq.com>
2022-07-19 19:40:57 +0800

    ssh push 1

--- src/main/java/com/gxh/git/TestGit.java
+++ src/main/java/com/gxh/git/TestGit.java
@@ -7,5 +7,6 @@ public class TestGit {
         System.out.println("hot-fix 1");
         System.out.println("master 1");
         System.out.println("hot-fix 2");
+        System.out.println("ssh push 1");
     }
 }
