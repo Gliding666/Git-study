354540ad3fc68e55cd4ccf4fae9f173dea6d5b27
Gliding666 <2225819610@qq.com>
2022-07-19 18:31:06 +0800

    second commit

--- src/main/java/com/gxh/git/TestGit.java
+++ src/main/java/com/gxh/git/TestGit.java
@@ -3,5 +3,6 @@ package com.gxh.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git 1");
+        System.out.println("hello git 2");
     }
 }
