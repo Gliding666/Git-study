1c7c29e3d4c5c1533380c95ccf2d22e7fda24b74
glide <glide@qq.com>
2023-06-07 15:34:30 +0800

    主分支合并新建分支的内容1

--- src/main/java/com/gxh/git/TestGit.java
+++ src/main/java/com/gxh/git/TestGit.java
@@ -5,9 +5,6 @@ public class TestGit {
         System.out.println("hello git 1");
         System.out.println("hello git 2");
         System.out.println("hot-fix 1");
-        System.out.println("master 1");
-        System.out.println("hot-fix 2");
-        System.out.println("ssh push 1");
-        System.out.println("pull test 1");
+        System.out.println("新建的分支添加的");
     }
 }
