c54286fba3340362acc63273779a94d5ea5e504d
glide <glide@qq.com>
2023-06-07 15:53:30 +0800

    新建分支写2

--- src/main/java/com/gxh/git/NewClass.java
+++ src/main/java/com/gxh/git/NewClass.java
@@ -4,5 +4,6 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("新建的分支");
         System.out.println("新建分支写1");
+        System.out.println("新建分支写2");
     }
 }
